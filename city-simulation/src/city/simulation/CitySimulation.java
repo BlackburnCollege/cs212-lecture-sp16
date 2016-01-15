bf7ed53452e2c6bbc30fc99d4afa5e2da65ff590
joshua.gross <joshua.gross@HU130F002.Blackburn.College>
2016-01-15 16:09:59 -0600

    initial version

---
+++ city-simulation/src/city/simulation/CitySimulation.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package city.simulation;
+
+/**
+ *
+ * @author joshua.gross
+ */
+public class CitySimulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numberOfIceCreamBars = 100;
+        Freezer freezer = new Freezer(numberOfIceCreamBars);
+        IceCreamBar bar = new IceCreamBar();
+        for (int i = 0; i <= numberOfIceCreamBars; i++) {
+           freezer.addStock(bar);
+        }
+    }
+    
+}
---
+++ city-simulation/src/city/simulation/Freezer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package city.simulation;
+
+/**
+ *
+ * @author joshua.gross
+ */
+public class Freezer {
+    private IceCreamBar [] stock;
+    private int indexOfStock = 0;
+    
+    public Freezer(int amountOfIceCreamBars) {
+        this.stock = new IceCreamBar[amountOfIceCreamBars];
+    }
+    
+    public void addStock(IceCreamBar bar) {
+        this.stock[this.indexOfStock] = bar;
+        this.indexOfStock++;
+    }
+    
+}
