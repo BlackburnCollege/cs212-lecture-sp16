25770e95eef869590f367cc1bc85f9b0be94590d
joshua.gross <joshua.gross@HU130F002.Blackburn.College>
2016-01-18 12:16:09 -0600

    moved everything into valid packages

--- city-simulation/src/edu/blackburn/cscourses/cs212/sp16/jbg/citysimulation/Freezer.java
+++ city-simulation/src/edu/blackburn/cscourses/cs212/sp16/jbg/citysimulation/Freezer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package city.simulation;
+package edu.blackburn.cscourses.cs212.sp16.jbg.citysimulation;
 
 /**
  *
