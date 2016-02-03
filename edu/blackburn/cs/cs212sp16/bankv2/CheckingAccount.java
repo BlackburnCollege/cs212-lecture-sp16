3920fb0001f2cd9fa37bc2dec68b288f4eddd8dd
joshua.gross <joshua.gross@HU130F002.Blackburn.College>
2016-02-03 11:51:30 -0600

    added new banking stuff that, yeah, doesn't compile

---
+++ edu/blackburn/cs/cs212sp16/bankv2/CheckingAccount.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212sp16.bankv2;
+
+/**
+ *
+ * @author joshua.gross
+ */
+public class CheckingAccount extends Account {
+    
+    public CheckingAccount(Money balance) {
+        super(balance);
+    }
+    
+}
