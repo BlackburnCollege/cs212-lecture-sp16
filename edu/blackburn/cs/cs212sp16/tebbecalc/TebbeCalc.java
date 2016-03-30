76588c2ff04d444b19df87b2019123aefb5db3d0
joshua.gross <joshua.gross@HU130F002.Blackburn.College>
2016-03-30 11:53:14 -0500

    started clicker

---
+++ edu/blackburn/cs/cs212sp16/tebbecalc/TebbeCalc.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212sp16.tebbecalc;
+
+import javafx.application.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import java.io.*;
+
+/**
+ *
+ * @author joshua.gross
+ */
+public class TebbeCalc extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        
+        Parent root = FXMLLoader.load(this.getClass().getResource("TebbeCalc.fxml"));        
+        Scene scene = new Scene(root, 300, 250);        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
