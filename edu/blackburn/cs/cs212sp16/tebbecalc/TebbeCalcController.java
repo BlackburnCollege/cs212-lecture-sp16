8451bc7a3eb4ca5e1812887828f64c2a830e6af7
joshua.gross <joshua.gross@HU130F002.Blackburn.College>
2016-03-30 12:20:57 -0500

    started clicker

---
+++ edu/blackburn/cs/cs212sp16/tebbecalc/TebbeCalcController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212sp16.tebbecalc;
+
+import java.net.*;
+import java.util.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+/**
+ * FXML Controller class
+ *
+ * @author joshua.gross
+ */
+public class TebbeCalcController implements Initializable {
+
+    @FXML
+    private TextField output;
+    
+    private int count = 0;
+
+    @FXML
+    private void handleClick(ActionEvent event) {
+        this.count++;
+        this.output.setText(Integer.toString(this.count));
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
