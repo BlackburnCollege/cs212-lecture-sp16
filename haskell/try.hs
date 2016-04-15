b3b652014a99a878adb5f56c1e220ff156ce65ca
jbgross <gross.joshua.b@gmail.com>
2016-04-15 01:49:10 -0500

    changed

--- haskell/try.hs
+++ haskell/try.hs
@@ -1,5 +1,6 @@
+import Debug.Trace
 --countsum :: Int a -> Int
 countsum x = if x > 0 then x + (countsum (x - 1)) else 0
-countdown x = if x > 0 then countdown (x -1) else 0
+countdown x = if x > 0 then trace("x is now " ++ show x) countdown (x -1) else 0
 makelist x = if x > 0 then x:(makelist (x-1)) else [0]
 --sumlist x = if (length x > 0) then (head x) + (sumlist (x-1)) else 0
