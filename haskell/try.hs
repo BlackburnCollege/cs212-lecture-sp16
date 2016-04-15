c47674c3e5e43280d32174c4fc2a23eaf9997470
jbgross <gross.joshua.b@gmail.com>
2016-04-15 01:37:54 -0500

    hopefully something useful

---
+++ haskell/try.hs
@@ -0,0 +1,5 @@
+--countsum :: Int a -> Int
+countsum x = if x > 0 then x + (countsum (x - 1)) else 0
+countdown x = if x > 0 then countdown (x -1) else 0
+makelist x = if x > 0 then x:(makelist (x-1)) else [0]
+--sumlist x = if (length x > 0) then (head x) + (sumlist (x-1)) else 0
