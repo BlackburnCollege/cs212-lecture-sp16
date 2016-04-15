28fd30f20fed09c8deff78939c14383386fdea0a
joshua.gross <joshua.gross@HU130G04.Blackburn.College>
2016-04-15 10:57:36 -0500

    added to infinity and beyond

--- haskell/try.hs
+++ haskell/try.hs
@@ -8,5 +8,6 @@ add5 (x:xs) = (trace("\nx is now "++ show x)) $ (if length xs > 0 then  (x + 5):
 add6 (x:xs) = if length xs > 0 then (x + 6):(add6 xs) else [x+6]
 add5' x = if length x > 0 then (head x + 5):(add5 (tail x)) else []
 addN (x:xs) y = if length xs > 0 then (x + y):(addN xs y) else [x+y]
+tiab x = (traceShow x) (tiab (x-1))
 -- will this work? why or why not?
 --sumlist x = if (length x > 0) then (head x) + (sumlist (x-1)) else 0
