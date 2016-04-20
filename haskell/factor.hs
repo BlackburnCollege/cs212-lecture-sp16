80d283290ea03db218b719c6d742dbf23c38e2bc
joshua.gross <joshua.gross@HU130F002.Blackburn.College>
2016-04-20 12:13:21 -0500

    fixed first bug, second one remains (hint, if you read this, think about the condition y = 2

--- haskell/factor.hs
+++ haskell/factor.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ImplicitParams #-}
 import Debug.Trace
 
-ifEven x
+isEven x
     | x `mod` 2 == 0 = True
     | otherwise = False
 
@@ -9,17 +9,33 @@ isEvenlyDivisibleBy x y
     | x `mod` y == 0 = True
     | otherwise = False
 
+isEven' x = isEvenlyDivisibleBy x 2
+
 bp x
     | isEvenlyDivisibleBy x 2 = 1
     | isEvenlyDivisibleBy x 5 = 5
     | otherwise = 0
+
+{-
+public class Bar...
+public void foo (int x, String y);
+public static void foo (int x, String y);
+
+Bar.foo(1, "Hunter");
+Bar b = new Bar...
+b.foo(1, "Hunter");
+
+private  int foo (int x, String y);
+
+public void foo (String y, int x);
+-}
 
 findFactors x = findFactors' x x
 
 -- this doesn't work - why not?
 findFactors' x y 
     | isEvenlyDivisibleBy x y = 
-        traceShow(x, y) $ 
+--        traceShow(x, y) $ 
         y:(findFactors' x (y-1))
     | y > 1 = findFactors' x (y-1)
     | otherwise = [1]
