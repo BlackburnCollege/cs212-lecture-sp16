f0ce91ae4880daa81a452e97db463dfa0aa48093
joshua.gross <joshua.gross@HU130G04.Blackburn.College>
2016-04-20 15:28:42 -0500

    the solution is to resolve what happens when y == 1, but no one reads these commit messages

--- haskell/factor.hs
+++ haskell/factor.hs
@@ -5,30 +5,11 @@ isEven x
     | x `mod` 2 == 0 = True
     | otherwise = False
 
+isEven' x = isEvenlyDivisibleBy x 2
+
 isEvenlyDivisibleBy x y 
     | x `mod` y == 0 = True
     | otherwise = False
-
-isEven' x = isEvenlyDivisibleBy x 2
-
-bp x
-    | isEvenlyDivisibleBy x 2 = 1
-    | isEvenlyDivisibleBy x 5 = 5
-    | otherwise = 0
-
-{-
-public class Bar...
-public void foo (int x, String y);
-public static void foo (int x, String y);
-
-Bar.foo(1, "Hunter");
-Bar b = new Bar...
-b.foo(1, "Hunter");
-
-private  int foo (int x, String y);
-
-public void foo (String y, int x);
--}
 
 findFactors x = findFactors' x x
 
